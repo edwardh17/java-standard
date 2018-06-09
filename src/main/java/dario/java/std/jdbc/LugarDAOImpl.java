d9eeeec921326a7def9cf59359cfda98acd4328f
EducaciónIT <EducacinIT@Instructor-607>
2018-06-09 17:20:44 -0300

    jdbc

---
+++ src/main/java/dario/java/std/jdbc/LugarDAOImpl.java
@@ -0,0 +1,69 @@
+package dario.java.std.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LugarDAOImpl implements LugarDAO {
+
+    @Override
+    public void grabar(Connection connection, Lugar lugar) throws SQLException {
+        PreparedStatement stmt = connection.prepareStatement("insert into lugar1 (id, NombreResponsable, Direccion, Router) values (?, ?,?,?) ");
+        stmt.setInt(1, lugar.getId());
+        stmt.setString(2, lugar.getNombreResponsable());
+        stmt.setString(3, lugar.getDireccion());
+        stmt.setString(4, lugar.getRouter());
+        stmt.executeUpdate();
+    }
+
+    @Override
+    public List<Lugar> obtenerTodos(Connection connection) throws SQLException {
+        List<Lugar> lugares = new ArrayList();
+        Statement smtm = connection.createStatement();
+
+        ResultSet result = smtm.executeQuery("select * from lugar1");
+
+        while (result.next()) {
+            int id = result.getInt("id");
+            String nombreResponsable = result.getString("NombreResponsable");
+            String direccion = result.getString("direccion");
+            String router = result.getString("Router");
+            lugares.add(new Lugar(id, nombreResponsable, direccion, router));
+        }
+        
+        return lugares;
+    }
+
+    @Override
+    public void actualizar(Connection connection, Lugar lugar)  throws SQLException {
+
+    }
+
+    @Override
+    public void borrar(Connection connection, Lugar lugar)  throws SQLException {
+
+    }
+
+    @Override
+    public Lugar buscarPorId(Connection connection, int id)throws SQLException {
+        Lugar lugar = null;
+            
+        PreparedStatement smtm = connection.prepareStatement("select * from lugar1 where id=?");
+        smtm.setInt(1, id);
+        ResultSet result = smtm.executeQuery();
+
+        if ( result.next()) {
+            String nombreResponsable = result.getString("NombreResponsable");
+            String direccion = result.getString("direccion");
+            String router = result.getString("Router");
+            lugar = new Lugar(id, nombreResponsable, direccion, router);
+        }
+        
+        return lugar;
+    }
+
+}
