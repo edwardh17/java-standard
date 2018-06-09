e932d891cd4fabf9d8b0ff0a2833afd4770c5f0f
EducaciónIT <EducacinIT@Instructor-607>
2018-06-09 18:22:31 -0300

    jdbc

--- src/main/java/dario/java/std/jdbc/LugarABMCController.java
+++ src/main/java/dario/java/std/jdbc/LugarABMCController.java
@@ -1,22 +1,76 @@
 package dario.java.std.jdbc;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author EducaciónIT
- */
 public class LugarABMCController implements Initializable {
+    
+    private LugarDAO dao;
+    private Connection connection;
+    
+    @FXML
+    private TextField idTxt;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private ListView<Lugar> listLugares;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtRouter;
+
+    @FXML
+    private Button btnGrabar;
+
+    @FXML
+    private TextField txtResponsable;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void grabar(ActionEvent event) {
+
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        try {
+            dao = new LugarDAOImpl();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bair", "root", "");
+            ObservableList<Lugar> items =FXCollections.observableArrayList (dao.obtenerTodos(connection));
+            listLugares.setItems(items);
+        } catch (SQLException ex) {
+            Logger.getLogger(LugarABMCController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ }    
     
 }
